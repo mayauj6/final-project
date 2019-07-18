cd9da0590a041c01d70ed485c365303e6ab95066
Apple <apple@Apples-MacBook-Pro-97.local>
2019-07-18 15:36:04 -0700

    post

--- post.js
+++ post.js
@@ -1,33 +1,35 @@
 var button = $('button');
 var container = $('.container')
-var arr = ["I am a victim","I am shocked","I am a survivor", "I am confident","I am amazing"];
+// var test = $('.test')
+var arr = [];
+var max = 10;
+var count = 0;
 
 button.on("click", post);
 
 
 //adds input to array and prints i of array
 function post(){
-  var count = 0;
   event.preventDefault();
-  var x = document.getElementById("myText").value;
-  console.log(x);
 
-  arr.pop();
-  arr.push("i am ${x}");
+
+  var text = $("#myText").val();
+  console.log(text);
+
+
+  arr.push(`i am ${text}`);
+
   console.log(arr);
 
-  container.append(`
-    <p>I am ${x}</p>
-    <p>${arr[0]}</p>
-    <p>${arr[1]}</p>
-    <p>${arr[2]}</p>
-    <p>${arr[3]}</p>
-    <p>${arr[4]}</p>
-    <p>${arr[5]}</p>
-    <p>${arr[6]}</p>
-    <p>${arr[7]}</p>
-    <p>${arr[8]}</p>
-  `)
 
+  if(arr.length >= max){
+     // document.getElementById(arr).remove;
+    arr.pop(0);
+    console.log(arr.length)
+  }
+  container.append(`
+    <p>${arr[count]}</p>
+  `
+  )
   count++;
-}
+  }
